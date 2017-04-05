d41a44f83d3741b42eaa27db1e7d7d614054c287
U-CORP\mhuang <mhuang@TN-MHUANG-1.corp.infusiondev.com>
2017-04-05 15:05:15 -0400

    first commit

---
+++ src/main/java/com/imarchuang/Java8TutApplication.java
@@ -0,0 +1,12 @@
+package com.imarchuang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8TutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8TutApplication.class, args);
+	}
+}
